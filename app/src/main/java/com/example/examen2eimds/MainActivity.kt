7381b1021f5f991e119cc95884fa4d0cabdecadf
byEdwin <edwinisaac@comunidad.unam.mx>
2026-05-27 20:42:46 -0600

    Diseño de vista de inicio de sesión

--- app/src/main/java/com/example/examen2eimds/MainActivity.kt
+++ app/src/main/java/com/example/examen2eimds/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.example.examen2eimds
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val etUsername = findViewById<EditText>(R.id.etUsername)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            val user = etUsername.text.toString()
+            if (user.isNotEmpty()) {
+                Toast.makeText(this, "Bienvenid@ $user", Toast.LENGTH_SHORT).show()
+            } else {
+                etUsername.error = "Ingresa tu nombre"
+            }
         }
     }
 }
